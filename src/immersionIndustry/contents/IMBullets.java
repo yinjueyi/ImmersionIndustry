0650edf9638ce54c3a54e9b58b3826605ffba7e3
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-17 22:49:59 +0800

    Add files via upload

---
+++ src/immersionIndustry/contents/IMBullets.java
@@ -0,0 +1,32 @@
+package immersionIndustry.contents;
+ 
+import arc.audio.*;
+import arc.math.geom.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import arc.struct.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.io.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+
+import static mindustry.Vars.*;
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+ 
+public class IMBullets implements ContentList {
+  
+	@Override
+	public void load() {
+	}
+}
