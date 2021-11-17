56f5c931ebfc94b1e946a860bf03db253b74a960
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-17 22:45:35 +0800

    Add files via upload

---
+++ src/immersionIndustry/IMColors.java
@@ -0,0 +1,7 @@
+package immersionIndustry;
+import arc.graphics.Color;
+
+public class IMColors {
+  public static Color colorPrimary = Color.valueOf("#8AA3F4"),
+  colorDarkPrimary = Color.valueOf("#6974C4"),colorYellow = Color.valueOf("#D3806A"),colorWhite = Color.valueOf("#FFFF8F");
+}
