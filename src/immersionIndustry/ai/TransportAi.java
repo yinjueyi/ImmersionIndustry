d0f5ff7995ae9b5835c62317cf81732193ca96e3
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-20 18:37:06 +0800

    Add files via upload

--- src/immersionIndustry/ai/TransportAi.java
+++ src/immersionIndustry/ai/TransportAi.java
@@ -11,9 +11,14 @@ import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
 
+import immersionIndustry.contents.IMFx;
+import immersionIndustry.IMColors;
+
 public class TransportAi extends AIController {
   
   Building from,to;
+  boolean wait;
+  float waitMultiple = 0.06f;
   
   public TransportAi(Building from,Building to) {
     this.from = from;
@@ -23,14 +28,20 @@ public class TransportAi extends AIController {
   
   @Override
   public void updateMovement() {
-    if(from == null || to == null) return;
+    if(from == null || to == null || wait) return;
     if(unit.stack.amount > 0) {
       //运送物品
       if(unit.inRange(to)) {
         int i = to.acceptStack(unit.stack.item,unit.stack.amount,unit);
         if(i > 0) {
-          to.handleStack(unit.stack.item,i,unit);
-          unit.stack.set(unit.stack.item,unit.stack.amount - i);
+          float waitTime = i < 600 ? 600 : i * waitMultiple;
+          wait = true;
+          IMFx.takeItemEffect(unit.x,unit.y,to.x,to.y,IMColors.colorYellow,waitTime);
+          Time.run(waitTime,() -> {
+            to.handleStack(unit.stack.item,i,unit);
+            unit.stack.set(unit.stack.item,unit.stack.amount - i);
+            wait = false;
+          });
         }
       }else {
         unit.movePref(vec.trns(unit.angleTo(to.x, to.y), unit.speed()));
@@ -38,8 +49,14 @@ public class TransportAi extends AIController {
     }else {
       Item item = from.items.first();
       if(unit.inRange(from) && item != null) {
-        int i = from.removeStack(item,unit.type.itemCapacity);
-        unit.stack.set(item,i);
+        float waitTime = i < 600 ? 600 : unit.stack.amount * waitMultiple;
+        wait = true;
+        IMFx.takeItemEffect(from.x,from.y,unit.x,unit.y,IMColors.colorPrimary,waitTime);
+        Time.run(waitTime,() -> {
+          int i = from.removeStack(item,unit.type.itemCapacity);
+          unit.stack.set(item,i);
+          wait = false;
+        });
       }else {
         unit.movePref(vec.trns(unit.angleTo(from.x, from.y), unit.speed()));
       }
