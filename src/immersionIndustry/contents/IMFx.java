c193a3f751aca614e1d3f02a41eb1a6d7c66bb6a
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-20 19:02:24 +0800

    Add files via upload

--- src/immersionIndustry/contents/IMFx.java
+++ src/immersionIndustry/contents/IMFx.java
@@ -46,12 +46,17 @@ public class IMFx implements ContentList {
       Draw.z(Layer.effect);
       Vec2 vec = new Vec2(e.x,e.y);
       vec.lerp(x2, y2, Interp.sineIn.apply(e.fin()));
+      color(color)
       Draw.alpha(e.fslope());
-      color(Color.white);
       Lines.stroke(2);
-      Lines.line(e.x,e.y,vec.x,vec.y);
-      color(color,Color.white,e.fslope());
-      Fill.square(vec.x, vec.y, 2);
+      Lines.line(e.x + tilesize,e.y,vec.x,vec.y);
+      Lines.line(e.x - tilesize,e.y,vec.x,vec.y);
+      
+      Fill.circle(vec.x, vec.y, 2 * e.fslope());
+      Draw.color();
+      Fill.circle(vec.x, vec.y, 1 * e.fslope());
+      
+      Drawf.dashLine(color,e.x,e.y,vec.x,vec.y);
     }).at(x,y);
   }
   
