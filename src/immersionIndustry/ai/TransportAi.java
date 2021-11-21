f35adfa505d57dacf04e6869b9529247204db7f4
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-21 10:45:43 +0800

    Add files via upload

--- src/immersionIndustry/ai/TransportAi.java
+++ src/immersionIndustry/ai/TransportAi.java
@@ -82,7 +82,7 @@ public class TransportAi extends AIController {
     for(int i = 0;i<4;i++) {
       Tile t = tile.nearby(i);
       if(t != null && t.floor().isLiquid && t.block() == Blocks.air) {
-        int d = manhattanDistance(tile.x,tile.y,to.tileX(),to.tileY());
+        int d = manhattanDistance(t.x,t.y,to.tileX(),to.tileY());
         if(value == -1 || value > d) {
           value = d;
           ti = t;
