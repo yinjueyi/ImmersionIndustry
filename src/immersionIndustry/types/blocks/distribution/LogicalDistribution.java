8fb58eb072c1db5dc6247ba9486a0695e796cac8
TOFU-AFK <72191075+TOFU-AFK@users.noreply.github.com>
2021-11-27 12:50:56 +0800

    Add files via upload

--- src/immersionIndustry/types/blocks/distribution/LogicalDistribution.java
+++ src/immersionIndustry/types/blocks/distribution/LogicalDistribution.java
@@ -58,37 +58,52 @@ public class LogicalDistribution extends Block {
     
     @Override
     public void buildConfiguration(Table table) {
+      setup(table);
+    }
+    
+    public void setup(Table table) {
+      table.clear();
+      float width = size * tilesize*4;
       table.table(Styles.black,cont -> {
         cont.add(getContName()+":");
         cont.row();
         cont.table(color -> {
           color.image().height(16).width(16).color(IMColors.colorPrimary);
+          color.add(new ItemDisplay(Item.copper,Item.lead));
           color.row();
           color.image().height(16).width(16).color(IMColors.colorYellow);
+          color.add(new ItemDisplay(Item.titanium,Item.silicon));
         }).pad(6).left();
         cont.row();
         cont.table(d -> {
           d.table(top -> {
             for(int i = 0; i<size;i++) {
-              top.add(new View(IMColors.colorPrimary,false)).size(size * tilesize*2);
+              top.add(new View(IMColors.colorPrimary,false)).size(width);
             }
           });
           d.row();
           d.table(left -> {
             for(int i = 0; i<size;i++) {
-              left.add(new View(IMColors.colorPrimary,false)).size(size * tilesize*2);
+              left.add(new View(IMColors.colorPrimary,false)).size(width);
+              left.row();
             }
           });
-          d.table(new TextureRegionDrawable(uiIcon)).size(size * tilesize*2);
+          d.table(new TextureRegionDrawable(uiIcon)).size(width);
           d.table(right -> {
             for(int i = 0; i<size;i++) {
-              right.add(new View(IMColors.colorPrimary,false)).size(size * tilesize*2);
+              right.add(new View(IMColors.colorPrimary,false)).size(width);
+              right.row();
             }
+            right.table(ext - > {
+              center();
+              ext.add("远程传输");
+              ext.add(new View(IMColors.colorPrimary,false)).size(width);
+            }).marginLeft(width*2);
           });
           d.row();
           d.table(bottom -> {
             for(int i = 0; i<size;i++) {
-              bottom.add(new View(IMColors.colorPrimary,false)).size(size * tilesize*2);
+              bottom.add(new View(IMColors.colorPrimary,false)).size(width);
             }
           });
         }).pad(6).center();
@@ -109,17 +124,8 @@ public class LogicalDistribution extends Block {
     public View(Color color,boolean export) {
       super();
       borderColor = color;
+      color = export ? IMColors.colorPrimary : colorYellow;
       this.export = export;
-    }
-    
-    @Override
-    public void draw(){
-      super.draw();
-      if(export) {
-        Fill.square(x,y,width/2);
-      }else {
-        Fill.circle(x,y,width/2);
-      }
     }
     
   }
